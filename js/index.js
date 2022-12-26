b8b72a1d045f61dd293e681e46c4eb5853974736
HeleneElena <heleneebert1@gmail.com>
2022-12-26 12:24:53 +0100

    feat: modal

--- js/index.js
+++ js/index.js
@@ -3,51 +3,57 @@ const modalProduct = document.querySelector('.modal_product'),
       countMinus = document.querySelector('.count__minus'),
       countAmount = document.querySelector('.count__amount'),
       countPlus = document.querySelector('.count__plus'),
-      catalogList = document.querySelector('.catalog__list');
+      catalogList = document.querySelector('.catalog__list'),
+      productImage = document.querySelector('.product__image');
 
-const modalProductTitle = document.querySelector('.modal-product__title');
-const modalProductImage = document.querySelector('.modal-product__image');
-const modalProductIngredients = document.querySelector('.modal-product__ingredients');
-const modalProductDescription = document.querySelector('.modal-product__description');
-const ingredientsList = document.querySelector('.ingredients__list');
-const ingredientsCalories = document.querySelector('.ingredients__calories');
-const productPrice = document.querySelector('.product__price');
+const modalProductTitle = document.querySelector('.modal-product__title'),
+      modalProductImage = document.querySelector('.modal-product__image'),
+      modalProductIngredients = document.querySelector('.modal-product__ingredients'),
+      modalProductDescription = document.querySelector('.modal-product__description'),
+      ingredientsCalories = document.querySelector('.ingredients__calories'),
+      ingredientsList = document.querySelector('.ingredients__list'),
+      productPrice = document.querySelector('.product__price');
 
-console.log(modalProductTitle);
-
-      const product = {
-        title: 'бургер Ура',
-        price: 222,
-        weight: 200,
-        calories: 1500,
-        description: 'большой классный бургер, самый популярный в нашем магазине',
-        image: 'img/burger4.jpg',
-        ingredients: ['brot', 'butter', 'chicken'],
-      };
+const product = {
+    title: 'бургер Ура',
+    price: 222,
+    weight: 200,
+    calories: 1500,
+    description: 'большой классный бургер, самый популярный в нашем магазине',
+    image: 'img/burger4.jpg',
+    ingredients: ['brot', 'butter', 'chicken'],
+};
 
 modalProductTitle.textContent = product.title;
 modalProductImage.src = product.image;
+modalProductDescription.textContent = product.description;
+ingredientsCalories.textContent = product.calories;
 productPrice.textContent = product.price;
 ingredientsList.textContent = '';
 
-for (let i = 0; i < product.ingredients.length; i++) {
+const iningredientsListItem = product.ingredients.map(el => {
     const li = document.createElement('li');
-    li.textContent = product.ingredients[i];
     li.classList.add('ingredients__item');
+    li.textContent = el;
     ingredientsList.append(li);
-}
+});
 
 
 catalogList.addEventListener('click', (e) => {
     let target = e.target;
-    if (target.closest('.product__detail') || target.closest('.product__image')) {
-            modalProduct.style.display = 'block';
-        }
+    if (target.closest('.product__title') || target.closest('.product__image')) {
+        modalProduct.classList.add('modal_open');
+    }
 });
 
 modalClose.addEventListener('click', (e) => {
-    modalProduct.style.display = 'none';
+    let target = e.target;
+    if (target.closest('.modal__close') || target.closest('.modal_product')) {
+        modalProduct.classList.remove('modal_open');
+    }
 });
+
+
 
 
 
