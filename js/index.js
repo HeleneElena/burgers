35e9c157887d8d3ee7202e81c22d566fdc7481fa
HeleneElena <heleneebert1@gmail.com>
2023-01-02 20:34:47 +0100

    fix: api

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import { catalogList, modalProduct } from "./elements.js";
 import { openModal } from './openModal.js';
 import { navigationListController } from './navigationListController.js';
+import { renderListProduct } from "./renderListProduct.js";
 
 const product = {
     title: 'бургер Oppa-pa',
@@ -12,34 +13,6 @@ const product = {
     ingredients: ['brot', 'butter', 'chicken'],
 };
 
-export const createCardProduct = (product) => {
-    const li = document.createElement('li');
-    li.classList.add('.catalog__item');
-    li.innerHTML = `
-        <article class="product">
-            <img class="product__image" src="${product.image}" alt="${product.title}">
-
-            <p class="product__price">${product.price}<span class="currency"> ₽</span></p>
-
-            <h3 class="product__title">
-                <button class="product__detail">${product.title}</button>
-            </h3>
-
-            <p class="product__weight">${product.weight} г</p>
-
-            <button class="product__add">Добавить</button>
-        </article>
-    `;
-    return li;
-};
-
-const cards = [
-    createCardProduct(product),
-    createCardProduct(product),
-    createCardProduct(product)
-];
-
-catalogList.append(...cards);
 
 catalogList.addEventListener('click', e => {
     const target = e.target;
@@ -59,7 +32,8 @@ modalProduct.addEventListener('click', e => {
 });
 
 const init = () => {
-    navigationListController();
+    renderListProduct();
+    navigationListController(renderListProduct);
 };
 init();
 
--- js/navigationListController.js
+++ js/navigationListController.js
@@ -4,14 +4,17 @@ import {
   catalogTitle
 } from './elements.js';
 
-export const navigationListController = () => {
+export const navigationListController = (cb) => {
     navigationList.addEventListener('click', e => {
     const listElem = e.target.closest('.navigation__button');
+
+    if (!listElem ) return;
 
       navigationItems.forEach(el => {
           if (el === listElem) {
               el.classList.add('navigation__button_active');
               catalogTitle.textContent = el.textContent;
+              cb(el.dataset.category);
           } else {
               el.classList.remove('navigation__button_active');
           }     
